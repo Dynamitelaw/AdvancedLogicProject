df9313076ca0c70fc2609fcb5ab80270ee40a3ec
Dynamitelaw <noobeater122333444455555@gmail.com>
2018-10-23 11:29:35 -0400

    Fixed dataset loading

--- MLP-MATLAB-master/mnistHelper/loadMNISTImages.m
+++ MLP-MATLAB-master/mnistHelper/loadMNISTImages.m
@@ -2,6 +2,7 @@ function images = loadMNISTImages(filename)
 %loadMNISTImages returns a 28x28x[number of MNIST images] matrix containing
 %the raw MNIST images
 
+disp(filename)
 fp = fopen(filename, 'rb');
 assert(fp ~= -1, ['Could not open ', filename, '']);
 

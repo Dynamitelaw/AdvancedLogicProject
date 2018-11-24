b9085d43210f5d2cb2e3dc0c042e6a0c7ea5f9ec
Dynamitelaw <noobeater122333444455555@gmail.com>
2018-11-23 23:41:43 -0500

    Fixed storage compilation issues. Started test bench

--- rtl/NeuralNetwork/GlobalVariables.v
+++ rtl/NeuralNetwork/GlobalVariables.v
@@ -2,8 +2,10 @@
 This file contains all global macro definitions
 */
 
+`define TRUE 1
+`define FALSE 0
 
-'define INPUT_LAYER_NODES 784
-'define LAYER_1_BIT_WIDTH 64  
+`define INPUT_LAYER_NODES 784
+`define LAYER_1_BIT_WIDTH 1  
 
-'define RELU_NODES 16
+`define RELU_NODES 16
---
+++ rtl/NeuralNetwork/TEST_Layer1WeightStorage.v
@@ -0,0 +1,47 @@
+/*
+ * Test bench for Layer1WeightStorage
+ */
+
+`include "GlobalVariables.v"
+
+
+module TEST_Layer1WeightStorage ;
+	reg writeEnable;
+	reg [9:0] NodeSelect;
+	reg [`RELU_NODES*`LAYER_1_BIT_WIDTH-1:0] writeIn;
+	wire [`RELU_NODES*`LAYER_1_BIT_WIDTH-1:0] readOut;
+
+	Layer1WeightStorage storage(
+		.writeEnable(writeEnable), 
+		.NodeSelect(NodeSelect), 
+		.writeIn(writeIn),
+		.readOut(readOut)
+	);
+
+	initial
+	begin
+		assign writeEnable = `FALSE;
+		assign NodeSelect = 0;
+		assign writeIn = 38;
+		#1
+		assign writeEnable = `TRUE;
+		#1
+		assign writeIn = 602;
+		#1
+		assign writeEnable = `FALSE;
+		assign writeIn = 52;
+		assign NodeSelect = 720;
+		#1
+		assign writeEnable = `TRUE;
+		#1
+		assign writeEnable = `FALSE;
+		#10
+		assign NodeSelect = 0;
+		#10
+		assign NodeSelect = 720;
+		$finish;
+
+	end
+	
+
+endmodule
